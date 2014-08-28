5dd65d5159eac1f91572d0f6366effada895368d
Alistair Miles <alimanfoo@googlemail.com>
2014-08-28 15:29:39 +0100

    rework allele frequencies module for greater flexibility

--- docs/af.rst
+++ docs/af.rst
@@ -2,20 +2,26 @@ Allele frequencies
 ==================
 
 .. automodule:: anhima.af
+.. autofunction:: max_allele
+.. autofunction:: allelism
+.. autofunction:: allele_number
+.. autofunction:: allele_count
+.. autofunction:: allele_frequency
+.. autofunction:: allele_counts
+.. autofunction:: allele_frequencies
 .. autofunction:: is_variant
 .. autofunction:: is_non_variant
+.. autofunction:: is_segregating
 .. autofunction:: is_non_segregating
 .. autofunction:: is_singleton
 .. autofunction:: is_doubleton
 .. autofunction:: count_variant
 .. autofunction:: count_non_variant
+.. autofunction:: count_segregating
 .. autofunction:: count_non_segregating
 .. autofunction:: count_singletons
 .. autofunction:: count_doubletons
-.. autofunction:: allele_number
-.. autofunction:: allele_count
-.. autofunction:: allele_frequency
-.. autofunction:: allele_frequencies
+
 
 
 
--- src/anhima/af.py
+++ src/anhima/af.py
@@ -22,6 +22,23 @@ import numpy as np
 import anhima.gt
 
 
+def _check_genotypes(genotypes):
+    """
+    Internal function to check the genotypes input argument meets
+    expectations.
+
+    """
+
+    genotypes = np.asarray(genotypes)
+    assert genotypes.ndim >= 2
+
+    if genotypes.ndim == 2:
+        # assume haploid, add ploidy dimension
+        genotypes = genotypes[..., np.newaxis]
+
+    return genotypes
+
+
 def is_variant(genotypes, min_ac=1):
     """Find variants with at least `min_ac` non-reference alleles.
 
@@ -54,9 +71,8 @@ def is_variant(genotypes, min_ac=1):
 
     """
 
-    # normalise inputs
-    genotypes = np.asarray(genotypes)
-    assert genotypes.ndim == 3
+    # check inputs
+    genotypes = _check_genotypes(genotypes)
 
     # aggregate over samples and ploidy dimensions
     out = np.sum(genotypes > 0, axis=(1, 2)) >= min_ac
@@ -125,9 +141,8 @@ def is_non_variant(genotypes):
 
     """
 
-    # normalise inputs
-    genotypes = np.asarray(genotypes)
-    assert genotypes.ndim == 3
+    # check inputs
+    genotypes = _check_genotypes(genotypes)
 
     # aggregate over samples and ploidy dimensions
     out = np.all(genotypes <= 0, axis=(1, 2))
@@ -165,83 +180,6 @@ def count_non_variant(genotypes):
     return np.count_nonzero(is_non_variant(genotypes))
 
 
-def is_non_segregating(genotypes, allele=1):
-    """Find non-segregating variants for the given allele.
-
-    Parameters
-    ----------
-
-    genotypes : array_like
-        An array of shape (`n_variants`, `n_samples`, `ploidy`) where each
-        element of the array is an integer corresponding to an allele index
-        (-1 = missing, 0 = reference allele, 1 = first alternate allele,
-        2 = second alternate allele, etc.).
-    allele : int, optional
-        The allele to test for fixation.
-
-    Returns
-    -------
-
-    is_non_segregating : ndarray, bool
-        An array of shape (`n_variants`,) where an element is True if all
-        genotype calls for the corresponding variant are either missing or
-        equal to `allele`.
-
-    Notes
-    -----
-
-    Applicable to polyploid genotype calls.
-
-    Applicable to multiallelic variants, although note that this function
-    tests for variants non-segregating with respect to the given `allele`,
-    it does not find non-segregating variants in general.
-
-    """
-
-    # normalise inputs
-    genotypes = np.asarray(genotypes)
-    assert genotypes.ndim == 3
-
-    # aggregate over samples and ploidy dimensions
-    out = np.all((genotypes < 0) | (genotypes == allele), axis=(1, 2))
-
-    return out
-
-
-def count_non_segregating(genotypes, allele=1):
-    """Count non-segregating variants for the given allele.
-
-    Parameters
-    ----------
-
-    genotypes : array_like
-        An array of shape (`n_variants`, `n_samples`, `ploidy`) where each
-        element of the array is an integer corresponding to an allele index
-        (-1 = missing, 0 = reference allele, 1 = first alternate allele,
-        2 = second alternate allele, etc.).
-    allele : int, optional
-        The allele to test for fixation.
-
-    Returns
-    -------
-
-    n : int
-        The number of variants.
-
-    Notes
-    -----
-
-    Applicable to polyploid genotype calls.
-
-    Applicable to multiallelic variants, although note that this function
-    tests for variants non-segregating with respect to the given `allele`,
-    it does not find non-segregating variants in general.
-
-    """
-
-    return np.count_nonzero(is_non_segregating(genotypes, allele))
-
-
 def is_singleton(genotypes, allele=1):
     """Find variants with only a single instance of `allele` called.
 
@@ -273,9 +211,8 @@ def is_singleton(genotypes, allele=1):
 
     """
 
-    # normalise inputs
-    genotypes = np.asarray(genotypes)
-    assert genotypes.ndim == 3
+    # check inputs
+    genotypes = _check_genotypes(genotypes)
 
     # aggregate over samples and ploidy dimensions
     out = np.sum(genotypes == allele, axis=(1, 2)) == 1
@@ -347,9 +284,8 @@ def is_doubleton(genotypes, allele=1):
 
     """
 
-    # normalise inputs
-    genotypes = np.asarray(genotypes)
-    assert genotypes.ndim == 3
+    # check inputs
+    genotypes = _check_genotypes(genotypes)
 
     # aggregate over samples and ploidy dimensions
     out = np.sum(genotypes == allele, axis=(1, 2)) == 2
@@ -418,9 +354,8 @@ def allele_number(genotypes):
 
     """
 
-    # normalise inputs
-    genotypes = np.asarray(genotypes)
-    assert genotypes.ndim == 3
+    # check inputs
+    genotypes = _check_genotypes(genotypes)
 
     # aggregate over samples and ploidy dimensions
     an = np.sum(genotypes >= 0, axis=(1, 2))
@@ -459,9 +394,8 @@ def allele_count(genotypes, allele=1):
 
     """
 
-    # normalise inputs
-    genotypes = np.asarray(genotypes)
-    assert genotypes.ndim == 3
+    # check inputs
+    genotypes = _check_genotypes(genotypes)
 
     # aggregate over samples and ploidy dimensions
     ac = np.sum(genotypes == allele, axis=(1, 2))
@@ -517,8 +451,35 @@ def allele_frequency(genotypes, allele=1):
     return an, ac, af
 
 
-def allele_frequencies(genotypes, alleles=(0, 1)):
-    """Calculate frequencies of the given alleles.
+def max_allele(genotypes, axis=None):
+    """
+    Return the highest allele index.
+
+    Parameters
+    ----------
+
+    genotypes : array_like
+        An array of shape (`n_variants`, `n_samples`, `ploidy`) where each
+        element of the array is an integer corresponding to an allele index
+        (-1 = missing, 0 = reference allele, 1 = first alternate allele,
+        2 = second alternate allele, etc.).
+    axis : int, optional
+        The axis along which to determine the maximum. If not given, return 
+        the highest overall.
+
+    Returns
+    -------
+
+    n : int
+        The value of the highest allele index present in the genotypes array.
+
+    """
+    
+    return np.amax(genotypes, axis=axis)
+
+
+def allele_counts(genotypes, alleles=None):
+    """Calculate allele counts.
 
     Parameters
     ----------
@@ -529,7 +490,58 @@ def allele_frequencies(genotypes, alleles=(0, 1)):
         (-1 = missing, 0 = reference allele, 1 = first alternate allele,
         2 = second alternate allele, etc.).
     alleles : sequence of ints, optional
-        The alleles to calculate the frequency of.
+        The alleles to calculate the frequency of. If not specified, all 
+        alleles will be counted.
+
+    Returns
+    -------
+
+    ac : ndarray, int
+        An array of shape (`n_variants`, `n_alleles`) counting the number of
+        times the given `alleles` were called.
+
+    Notes
+    -----
+
+    Applicable to polyploid genotype calls.
+
+    Applicable to multiallelic variants.
+
+    """
+
+    # check input
+    genotypes = _check_genotypes(genotypes)
+
+    # determine number of variants
+    n_variants = genotypes.shape[0]
+
+    # if alleles not specified, count all alleles
+    if alleles is None:
+        m = max_allele(genotypes)
+        alleles = range(m+1)
+
+    # count alleles
+    ac = np.zeros((n_variants, len(alleles)), dtype='i4')
+    for i, a in enumerate(alleles):
+        ac[:, i] = allele_count(genotypes, allele=a)
+
+    return ac
+    
+
+def allele_frequencies(genotypes, alleles=None):
+    """Calculate allele frequencies.
+
+    Parameters
+    ----------
+
+    genotypes : array_like
+        An array of shape (`n_variants`, `n_samples`, `ploidy`) where each
+        element of the array is an integer corresponding to an allele index
+        (-1 = missing, 0 = reference allele, 1 = first alternate allele,
+        2 = second alternate allele, etc.).
+    alleles : sequence of ints, optional
+        The alleles to calculate the frequency of. If not specified, all 
+        alleles will be counted.
 
     Returns
     -------
@@ -538,10 +550,10 @@ def allele_frequencies(genotypes, alleles=(0, 1)):
         An array of shape (`n_variants`,) counting the total number of
         non-missing alleles called.
     ac : ndarray, int
-        An array of shape (`n_variants`, `len(alleles)`) counting the number of
+        An array of shape (`n_variants`, `n_alleles`) counting the number of
         times the given `alleles` were called.
     af : ndarray, float
-        An array of shape (`n_variants`, `len(alleles)`) containing the allele
+        An array of shape (`n_variants`, `n_alleles`) containing the allele
         frequencies.
 
     Notes
@@ -549,28 +561,214 @@ def allele_frequencies(genotypes, alleles=(0, 1)):
 
     Applicable to polyploid genotype calls.
 
-    Applicable to multiallelic variants, but note that this function
-    calculates the frequency of a specific `alleles`.
+    Applicable to multiallelic variants.
 
     """
-
-    # normalise inputs
-    genotypes = np.asarray(genotypes)
-    assert genotypes.ndim == 3
-
-    # set up output arrays
-    n_variants = genotypes.shape[0]
-    n_alleles = len(alleles)
-    ac = np.empty((n_variants, n_alleles), dtype='i4')
-    af = np.empty((n_variants, n_alleles), dtype='f4')
 
     # count non-missing alleles
     an = allele_number(genotypes)
 
-    # loop over alleles
-    for n, allele in enumerate(alleles):
-        ac[:, n] = allele_count(genotypes, allele=allele)
-        af[:, n] = ac[:, n] / an
+    # count alleles
+    ac = allele_counts(genotypes, alleles=alleles)
+
+    # calculate allele frequencies
+    af = ac / an[..., np.newaxis]
 
     return an, ac, af
 
+
+def allelism(genotypes):
+    """Determine the number of distinct alleles found for each variant.
+
+    Parameters
+    ----------
+
+    genotypes : array_like
+        An array of shape (`n_variants`, `n_samples`, `ploidy`) where each
+        element of the array is an integer corresponding to an allele index
+        (-1 = missing, 0 = reference allele, 1 = first alternate allele,
+        2 = second alternate allele, etc.).
+
+    Returns
+    -------
+
+    n : ndarray, int
+        An array of shape (`n_variants`,) where an element holds the allelism 
+        of the corresponding variant.
+
+    See Also
+    --------
+
+    max_allele
+
+    Notes
+    -----
+
+    Applicable to polyploid genotype calls.
+
+    Applicable to multiallelic variants.
+
+    """
+    
+    # check inputs
+    genotypes = _check_genotypes(genotypes)
+
+    # calculate allele counts
+    ac = allele_counts(genotypes)
+
+    # count alleles present
+    n = np.sum(ac > 0, axis=1)
+
+    return n
+
+
+def is_non_segregating(genotypes, allele=None):
+    """Find non-segregating variants (fixed for a single allele).
+
+    Parameters
+    ----------
+
+    genotypes : array_like
+        An array of shape (`n_variants`, `n_samples`, `ploidy`) where each
+        element of the array is an integer corresponding to an allele index
+        (-1 = missing, 0 = reference allele, 1 = first alternate allele,
+        2 = second alternate allele, etc.).
+    allele : int, optional
+        If given, find variants fixed with respect to `allele`.
+
+    Returns
+    -------
+
+    is_non_segregating : ndarray, bool
+        An array of shape (`n_variants`,) where an element is True if all
+        genotype calls for the corresponding variant are either missing or
+        equal to the same allele.
+
+    Notes
+    -----
+
+    Applicable to polyploid genotype calls.
+
+    Applicable to multiallelic variants.
+
+    """
+
+    # check inputs
+    genotypes = _check_genotypes(genotypes)
+
+    if allele is None:
+
+        # count distinct alleles
+        n_alleles = allelism(genotypes)
+
+        # find fixed variants
+        out = n_alleles == 1
+
+    else:
+
+        # find fixed variants with respect to a specific allele
+        out = np.all((genotypes < 0) | (genotypes == allele), axis=1)
+
+    return out
+
+
+def count_non_segregating(genotypes, allele=None):
+    """Count non-segregating variants.
+
+    Parameters
+    ----------
+
+    genotypes : array_like
+        An array of shape (`n_variants`, `n_samples`, `ploidy`) where each
+        element of the array is an integer corresponding to an allele index
+        (-1 = missing, 0 = reference allele, 1 = first alternate allele,
+        2 = second alternate allele, etc.).
+    allele : int, optional
+        If given, find variants fixed with respect to `allele`.
+
+    Returns
+    -------
+
+    n : int
+        The number of variants.
+
+    Notes
+    -----
+
+    Applicable to polyploid genotype calls.
+
+    Applicable to multiallelic variants.
+
+    """
+
+    return np.count_nonzero(is_non_segregating(genotypes, allele=allele))
+
+
+def is_segregating(genotypes):
+    """Find segregating variants (where more than one allele is found).
+
+    Parameters
+    ----------
+
+    genotypes : array_like
+        An array of shape (`n_variants`, `n_samples`, `ploidy`) where each
+        element of the array is an integer corresponding to an allele index
+        (-1 = missing, 0 = reference allele, 1 = first alternate allele,
+        2 = second alternate allele, etc.).
+
+    Returns
+    -------
+
+    is_segregating : ndarray, bool
+        An array of shape (`n_variants`,) where an element is True if more 
+        than one allele is found for the given variant.
+
+    Notes
+    -----
+
+    Applicable to polyploid genotype calls.
+
+    Applicable to multiallelic variants.
+
+    """
+
+    # check inputs
+    genotypes = _check_genotypes(genotypes)
+
+    # count distinct alleles
+    n_alleles = allelism(genotypes)
+
+    # find segregating variants
+    out = n_alleles > 1
+
+    return out
+
+
+def count_segregating(genotypes):
+    """Count segregating variants.
+
+    Parameters
+    ----------
+
+    genotypes : array_like
+        An array of shape (`n_variants`, `n_samples`, `ploidy`) where each
+        element of the array is an integer corresponding to an allele index
+        (-1 = missing, 0 = reference allele, 1 = first alternate allele,
+        2 = second alternate allele, etc.).
+
+    Returns
+    -------
+
+    n : int
+        The number of variants.
+
+    Notes
+    -----
+
+    Applicable to polyploid genotype calls.
+
+    Applicable to multiallelic variants.
+
+    """
+
+    return np.count_nonzero(is_segregating(genotypes))
