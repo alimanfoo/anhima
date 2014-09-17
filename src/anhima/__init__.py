69517dd8532cdacd7aa3182f72ea178d65132f7a
Alistair Miles <alimanfoo@googlemail.com>
2014-09-17 13:13:30 +0100

    increment version

--- src/anhima/__init__.py
+++ src/anhima/__init__.py
@@ -8,7 +8,7 @@ from __future__ import division, print_function, unicode_literals, \
     absolute_import
 
 
-__version__ = '0.8'
+__version__ = '0.9-SNAPSHOT'
 
 
 # make sub-packages available without explicit import
