8475740708d997d6550cb45d38a36caecc1c1085
Alistair Miles <alimanfoo@googlemail.com>
2014-10-20 11:25:15 +0100

    v0.10

--- src/anhima/__init__.py
+++ src/anhima/__init__.py
@@ -11,7 +11,7 @@ from __future__ import division, print_function, unicode_literals, \
     absolute_import
 
 
-__version__ = '0.10-SNAPSHOT'
+__version__ = '0.10'
 
 
 # make sub-packages available without explicit import
